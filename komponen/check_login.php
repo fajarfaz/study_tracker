f2638269160dea5e8d9b921389702a4c95ecddaa
fajarfaz <fajarfaz@gmail.com>
2021-04-14 13:40:32 +0700

    dash user

--- komponen/check_login.php
+++ komponen/check_login.php
@@ -1,22 +1,25 @@
 <?php
-include "koneksi.php";
+if(isset($_POST['do_login']))
+{
+    include "koneksi.php";
+$email=$_POST['email'];
+ $pass=$_POST['password'];
+ $query="SELECT * FROM login WHERE username='$email' AND password='$pass'";
+ $result = mysqli_query($conn,$query)or die(mysqli_error());
+ if($row=mysqli_fetch_array($result))
+ {
+   
+  $_SESSION['username']=$row['username'];
+  echo "success";
+   // session_start();
 
-$uname = mysqli_real_escape_string($conn,$_POST['username']);
-$password = mysqli_real_escape_string($conn,$_POST['password']);
-
-if ($uname != "" && $password != ""){
-
-    $sql_query = "select count(*) as cntUser from login where username='".$uname."' and password='".$password."'";
-    $result = mysqli_query($conn,$sql_query);
-    $row = mysqli_fetch_array($result);
-
-    $count = $row['cntUser'];
-
-    if($count > 0){
-        $_SESSION['uname'] = $uname;
-        echo 1;
-    }else{
-        echo 0;
-    }
-
+ }
+ else
+ {
+  echo "fail";
+ }
+ exit();
 }
+
+
+?>
--- komponen/koneksi.php
+++ komponen/koneksi.php
@@ -13,5 +13,4 @@ if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
 
-mysqli_close($conn);
 ?>
